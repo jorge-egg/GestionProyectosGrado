ed2edff76654963544aa4e17e679208b0d7043c9
YersonAG <yersonaguilarr@gmail.com>
2023-10-23 11:50:50 -0500

    correcion

--- database/seeders/GruposSeeder.php
+++ database/seeders/GruposSeeder.php
@@ -17,24 +17,20 @@ class GruposSeeder extends Seeder
             'numerogrupo' => 'Grupo 1',
             'estado'=>'activo',
             'cron_fech'=>1,
-            
             ]);
             DB::table('cronograma_grupos')->insert([
                 'numerogrupo' => 'Grupo 2',
                 'estado'=>'activo',
-                'cron_fech'=>1,
                 'cron_fech'=>1,
                 ]);
                 DB::table('cronograma_grupos')->insert([
                     'numerogrupo' => 'Grupo 3',
                     'estado'=>'activo',
                     'cron_fech'=>1,
-                    'cron_fech'=>1,
                     ]);
                     DB::table('cronograma_grupos')->insert([
                         'numerogrupo' => 'Grupo 4',
                         'estado'=>'activo',
-                        'cron_fech'=>1,
                         'cron_fech'=>1,
                         ]);
     }
