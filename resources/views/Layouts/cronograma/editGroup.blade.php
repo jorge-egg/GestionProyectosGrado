9e07f23e4c7c9e21ed2db5c07a24d81c29124481
YersonAG <yersonaguilarr@gmail.com>
2023-10-27 18:07:05 -0500

    calendario

--- resources/views/Layouts/cronograma/editGroup.blade.php
+++ resources/views/Layouts/cronograma/editGroup.blade.php
@@ -7,7 +7,24 @@
 @stop
 
 @section('dashboard_content')
-
+<div class="card">
+  <h5 class="card-header">EDITAR CRONOGRAMA</h5>
+  <div class="card-body">
+    <p class="card-text">
+        <form action="#">
+        <div><label for="">Fecha de apertura</label>
+            <input type="date" name='fecha_cierre' class='form-control' required>
+            </div>
+            <br>
+            <div><label for="">Fecha de cierre</label>
+            <input type="date" name='fecha_cierre' class='form-control' required>
+            </div>
+            <br>
+            <button Class='btn btn-outline-info'>editar</button>
+        </form>
+    </p>
+  </div>
+</div>
 
 
 @stop
