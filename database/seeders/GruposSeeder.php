6a97d6cad79d9ad93f52bb205f4e5bfef68807b9
YersonAG <yersonaguilarr@gmail.com>
2023-10-31 17:38:13 -0500

    grupos

--- database/seeders/GruposSeeder.php
+++ database/seeders/GruposSeeder.php
@@ -14,22 +14,18 @@ class GruposSeeder extends Seeder
     public function run()
     {
         DB::table('cronograma_grupos')->insert([
-            'numerogrupo' => 'Grupo 1',
             'estado'=>'activo',
             'cron_fech'=>1,
             ]);
             DB::table('cronograma_grupos')->insert([
-                'numerogrupo' => 'Grupo 2',
                 'estado'=>'activo',
                 'cron_fech'=>1,
                 ]);
                 DB::table('cronograma_grupos')->insert([
-                    'numerogrupo' => 'Grupo 3',
                     'estado'=>'activo',
                     'cron_fech'=>1,
                     ]);
                     DB::table('cronograma_grupos')->insert([
-                        'numerogrupo' => 'Grupo 4',
                         'estado'=>'activo',
                         'cron_fech'=>1,
                         ]);
