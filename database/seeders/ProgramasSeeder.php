98cd7ab350ad3982e2bb6d9895e5a9ef595783cb
YersonAG <138827038+YersonAG@users.noreply.github.com>
2023-11-08 21:35:02 -0500

    Update ProgramasSeeder.php

--- database/seeders/ProgramasSeeder.php
+++ database/seeders/ProgramasSeeder.php
@@ -19,21 +19,21 @@ class ProgramasSeeder extends Seeder
             'siglas'=>'ING',
             'prog_facu'=>1,
             'prog_sede'=>1,
-            'prog_usua'=>100972736,
+            
             ]);
             DB::table('sede_programas')->insert([
                 'programa' => 'diseño grafico',
                 'siglas'=>'DG',
                 'prog_facu'=>1,
                 'prog_sede'=>1,
-                'prog_usua'=>133978936,
+                
                 ]);
                 DB::table('sede_programas')->insert([
                     'programa' => 'contaduria publica',
                     'siglas'=>'CP',
                     'prog_facu'=>1,
                     'prog_sede'=>1,
-                    'prog_usua'=>748392749,
+                    
                     ]);
     }
 }
