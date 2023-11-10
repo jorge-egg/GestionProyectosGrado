8396d90cdf54d7a12c90f595443e1e6c664c1cea
Sebastian Rosero <srosero87@misena.edu.co>
2023-11-09 23:11:09 -0500

    Feat: se avanza en CRUD de propuesta, se crea validacion de js para inputs longitud de palabras

--- resources/views/Layouts/propuesta/create.blade.php
+++ resources/views/Layouts/propuesta/create.blade.php
@@ -8,20 +8,23 @@
         <form action="{{route('propuesta.store')}}" method='POST'>
         @csrf
         <div><label for="">titulo</label>
-            <input type="text" name='Titulo' class='form-control' required>
+            <input type="text" name='Titulo' id="titleForPropuestaId" oninput="limitarLongitud( this.id, 18, 'contadorTitle' )" class='form-control' required>
+            <p>Longitud máxima: <span id="contadorTitle"></span></p>
             </div>
             <div><label for="">Linea de investigacion</label>
             <input type="text" name='linea_invs' class='form-control' required>
             </div>
             <br>
             <div class="form-floating">
-                <textarea class="form-control" placeholder="Leave a comment here" name="desc_problema" required></textarea>
+                <textarea class="form-control" id="descriptionPropuestaId" oninput="limitarLongitud( this.id, 600, 'DescripcionContador' )" placeholder="Leave a comment here" name="desc_problema" required></textarea>
                 <label for="">Descripción del problema</label>
+                <p>Longitud máxima: <span id="DescripcionContador"></span></p>
             </div>
             <br>
             <div class="form-floating">
-                <textarea class="form-control" placeholder="Leave a comment here" name="obj_general" required></textarea>
+                <textarea class="form-control" id="objectiveGeneralId" placeholder="Leave a comment here" oninput="limitarLongitud( this.id, 25, 'ObjetivoGeneralContador' )" name="obj_general" required></textarea>
                 <label for="">Objetivo general</label>
+                <p>Longitud máxima: <span id="ObjetivoGeneralContador"></span></p>
             </div>
             <br>
             <div class="form-floating">
@@ -45,4 +48,29 @@
 
     </div>
 </div>
+
+@section('js')
+<script>
+    const limitarLongitud = ( id, longitud, contadorId ) => {
+        const input = document.getElementById( id );
+        const contador = document.getElementById( contadorId );
+
+        if( input.value.length <= 0 ){
+            contador.textContent = 0;
+            return;
+        }
+        
+        const maxPalabras = longitud; // Define la cantidad máxima de palabras aquí
+        
+        let palabras = input.value.split(' ');
+
+        if (palabras.length > maxPalabras) {
+            palabras.pop();
+            input.value = palabras.join(' ');
+        }
+
+        contador.textContent = palabras.length;
+    }
+</script>
+@endsection
 @stop
