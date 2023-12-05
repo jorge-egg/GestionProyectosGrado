fc57bfb3af81ad0f41049e0a34a9f15eba711420
jorge-egg <jorgedu0310@gmail.com>
2023-12-05 16:43:23 -0500

    style:view propuestaAll

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,6 @@
 namespace App\Models;
 use Spatie\Permission\Traits\HasRoles;
 use Carbon\Carbon;
-use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\SoftDeletes;
