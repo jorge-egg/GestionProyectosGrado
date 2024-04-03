b827966d2bc2278ca0ba35f6f209a2bdabdbeffd
YersonAG <yersonaguilarr@gmail.com>
2024-04-03 13:40:53 -0500

    tabla

--- resources/views/Layouts/proyecto/tableindex.blade.php
+++ resources/views/Layouts/proyecto/tableindex.blade.php
@@ -6,11 +6,20 @@
 @section('dashboard_content')
 
 <h1>Proyectos</h1>
+
+<div class="alert alert-info" role="alert">
+    <span style="color: rgb(0, 105, 0);">■</span> Aprobado &nbsp;&nbsp;
+    <span style="color: rgb(135, 0, 0);">■</span> Rechazado &nbsp;&nbsp;
+    <span style="color: rgb(171, 171, 0);">■</span> Aplazado &nbsp;&nbsp;
+    <span style="color: rgb(0, 0, 89);">■</span> Pendiente
+</div>
+
 <table class="table table-hover shadow-lg mt-4" style="width:100%" id='table-js'>
         <thead>
             <tr>
                 <th scope="col">estado</th>
                 <th scope="col">codigo proyecto</th>
+                <th scope="col">integrante</th>
                 <th scope="col"></th>
                 <th scope="col"></th>
                 <th scope="col"></th>
@@ -26,6 +35,7 @@
                         Finalizado
                     @endif</td>
                     <td>{{ $proyecto->codigoproyecto }}</td>
+                    <td>{{ $proyecto->usuario }}</td>
                     <td>
                         <form action="{{ route('propuesta.create', $proyecto->idProyecto) }}" method="post">
                             @csrf
