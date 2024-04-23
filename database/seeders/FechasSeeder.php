1379437eb8b2c399045239ffec9674d5f287b01b
jorge-egg <jorgedu0310@gmail.com>
2024-04-23 18:36:16 -0500

    Ajuste en seeder de fecha

--- database/seeders/FechasSeeder.php
+++ database/seeders/FechasSeeder.php
@@ -16,26 +16,26 @@ class FechasSeeder extends Seeder
 public function run()
 {
     DB::table('fechas_grupos')->insert([
-        'fecha_apertura' => '2023-03-08',
-        'fecha_cierre' => '2023-03-11',
+        'fecha_apertura' => '2024-04-20',
+        'fecha_cierre' => '2024-04-30',
         'fech_grup' => 1,
         'fech_fase' => 1,
     ]);
     DB::table('fechas_grupos')->insert([
-        'fecha_apertura' => '2023-04-19',
-        'fecha_cierre' => '2023-04-21',
+        'fecha_apertura' => '2024-04-20',
+        'fecha_cierre' => '2024-04-30',
         'fech_grup' => 1,
         'fech_fase' => 2,
     ]);
     DB::table('fechas_grupos')->insert([
-        'fecha_apertura' => '2023-05-03',
-        'fecha_cierre' => '2023-05-05',
+        'fecha_apertura' => '2024-05-03',
+        'fecha_cierre' => '2024-05-30',
         'fech_grup' => 1,
         'fech_fase' => 3,
     ]);
     DB::table('fechas_grupos')->insert([
-        'fecha_apertura' => '2023-09-11',
-        'fecha_cierre' => '2023-09-15',
+        'fecha_apertura' => '2024-05-11',
+        'fecha_cierre' => '2024-05-30',
         'fech_grup' => 1,
         'fech_fase' => 4,
     ]);
